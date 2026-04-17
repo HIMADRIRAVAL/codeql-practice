61f1d48889c2a75aa5f46a197046217ba8873c2c
HIMADRIRAVAL <164208321+HIMADRIRAVAL@users.noreply.github.com>
2026-04-17 19:20:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import os
+import subprocess
+import sqlite3
 
-user = input("Enter command: ")
-os.system(user)
+user = input("Name: ")
+query = "SELECT * FROM users WHERE name='" + user + "'"
+
+conn = sqlite3.connect("test.db")
+cursor = conn.cursor()
+cursor.execute(query)
+
+cmd = input("Command: ")
+os.system(cmd)
